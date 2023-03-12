f094d308241b0b83535fce380895f320d6ffc8cb
PighouseBacon <pighousebacon@gmail.com>
2023-03-12 16:38:55 -0700

    Initial Commit

---
+++ functions.lua
@@ -0,0 +1,209 @@
+function oops (id, other)
+    if id == 0 then
+        --help message
+        if input[2] == "upgrade" or input[2] == "upgrades" then
+            print("description of upgrades")
+        else
+            print("This is an incremental game using NotSoBot tag scripting.")
+            print("To play, each time you use the tag, you will get a code.")
+            print("You must then add the code to the end of your next use of the tag.")
+            print("For example, using the tag correctly would look something like this: ``{prefix}t incremental fe12be [command]``")
+            print("Each use of the tag calculates production equal to the time between the previous use of the tag and now,")
+            print("as well as adding additional resources equal to your \"click\" value.\n")
+            --[[
+            print("There are some commands you can use by appending them after the code as well.")
+            print("The full list of commands you can use is as follows:")
+            print("buy [val] [id]:   buys [val] of building [id]")
+            print("sell [val] [id]:   sells [val] of building [id]")
+            print("buy upgrade:   buys the next available upgrade, if it is affordable")
+            print("coop add [id]:   allows another user to play this savefile")
+            print("coop remove [id]:   removes a user from playing this savefile")
+            print("coop list:   lists the current users who can play this savefile")
+            print("You can also do ``.t incremental help [subject]`` for a description of a part in the game.\n")
+            --]]
+            print("To start playing, use the following code:")
+            print(genCode())
+        end
+    elseif id == 1 then
+        --malformed input
+        print("Malformed Input")
+        print(other)
+    elseif id == 2 then
+        print("You're not in this savefile's coop!")
+    elseif id == 3 then
+        print("Under maintenance! Please try again later.")
+    end
+    os.exit()
+end
+
+function genCode ()
+    time = tostring(os.time())
+    id = "{id}"
+    message = time .. ",0,{0,0},{false,false,false,false,false,false,false,false},{" .. id .. "},{" .. time .. ",0},"
+    return encode(message)
+end
+
+function split (str, splitter, maxDepth)
+    result = {}
+    depth = 0
+    cur = ""
+    for i = 1, #str do
+        char = str:sub(i, i)
+        if char == splitter and (maxDepth == nil or depth <= maxDepth) then
+            table.insert(result, cur)
+            cur = ""
+        else
+            if char == "{" then
+                depth = depth + 1
+            elseif char == "}" then
+                depth = depth - 1
+            end
+            cur = cur .. char
+        end
+    end
+    return result
+end
+
+function tabletostring (table)
+    message = ""
+    for i = 1, #table do
+        if type(table[i]) == "table" then
+            message = message .. "{"
+            for k = 1, #table[i] do
+                message = message .. tostring(table[i][k]) .. ","
+            end
+            message = message:sub(1, #message - 1) .. "}"
+        else
+            message = message .. tostring(table[i])
+        end
+        message = message .. ","
+    end
+    return message
+end
+
+--[[ old sciformat: had to change this due to requiring less total lines in the iscript
+function sciformat (val)
+    if val == math.huge then
+        return "!"
+    end
+    exponent = math.floor(math.log(val, 10))
+    if exponent >= 3 then
+        mantissa = math.floor(val / (10^(exponent - 2)))
+        mantissa = tostring(mantissa / 100)
+        return (mantissa .. "e" .. tostring(exponent))
+    end
+    return tostring(val)
+end
+--]]
+
+function sciformat (val)
+    if val == math.huge then
+        return "!"
+    end
+    exponent = math.floor(math.log(val, 10))
+    if exponent >= 4 then
+        if exponent >= 100 then
+            mantissa = ""
+        else
+            mantissa = math.floor(val / (10^exponent))
+        end
+        return (mantissa .. "e" .. tostring(exponent))
+    end
+    return tostring(val)
+end
+
+function makeText (val, x, y)
+    text = ""
+    cur = sciformat(val)
+    for i = 1, #cur do
+        char = (#cur - i) + 1
+        char = cur:sub(char, char)
+        text = text .. "\noverlay cropped spritesheet " .. locs[char] .. " 0"
+        text = text .. "\noverlay template cropped " .. tostring(x - (48 * i)) .. " ".. tostring(y)
+    end
+    return text
+end
+
+function encode (message)
+    encoded = ""
+    for i = 1, #message do
+        val = ("0" .. string.format("%x", (message:byte(i) + i) % 256)):sub(-2)
+        encoded = encoded .. val
+    end
+    return encoded
+end
+
+function decode (message)
+    if message == "" or message == "help" then
+        oops(0)
+    end
+
+    --decode hex into text
+    decoded = ""
+    for i = 1, #message, 2 do
+        val = tonumber(message:sub(i, i + 1), 16)
+        if val == nil then
+            oops(1, 71)
+        end
+        val = string.char((val - ((i + 1) / 2)) % 256)
+        decoded = decoded .. val
+    end
+    
+    --split outermost layer by commas
+    state = split(decoded, ",", 0)
+    if #state ~= 6 then
+        oops(1, 80)
+    end
+
+    --check each element in state for correct type
+    state[1] = tonumber(state[1])
+    state[2] = tonumber(state[2])
+    if state[1] == nil or state[2] == nil then
+        oops(1, 87)
+    end
+
+    if state[4]:sub(1, 1) ~= "{" or state[4]:sub(#state[4], #state[4]) ~= "}" then
+        oops(1, 91)
+    end
+    ups = split(state[4]:sub(2, #state[4] - 1) .. ",", ",")
+    for i = 1, #ups do
+        if ups[i] == "true" then
+            ups[i] = true
+        elseif ups[i] == "false" then
+            ups[i] = false
+        else
+            oops(1, 100)
+        end
+    end
+    state[4] = ups
+
+    for i = 1, 3 do
+        index = {3, 5, 6}
+        index = index[i]
+        if state[index]:sub(1, 1) ~= "{" or state[index]:sub(#state[index], #state[index]) ~= "}" then
+            oops(1, 109)
+        end
+        vals = split(state[index]:sub(2, #state[index] - 1) .. ",", ",")
+        for k = 1, #vals do
+            vals[k] = tonumber(vals[k])
+            if vals[k] == nil then
+                oops(1, 115)
+            end
+        end
+        state[index] = vals
+    end
+
+    user = tonumber("{id}")
+    found = false
+    for i = 1, #state[5] do
+        if user == state[5][i] then
+            found = true
+            break
+        end
+    end
+    if not found then
+        oops(2)
+    end
+
+    return state
+end
---
+++ main.lua
@@ -0,0 +1,127 @@
+print("Version 27")
+
+if "{id}" ~= "221158868938522624" then
+    oops(3)
+end
+
+input = split("{args} ", " ")
+--[[
+for key,value in pairs(input) do
+    print(value)
+end
+print("\n")
+--]]
+
+game = decode(input[1])
+--now..simulate tick(s)
+multipliers = {1, 1, 1}
+if game[4][1] then
+    multipliers[1] = multipliers[1] * 2
+end
+if game[4][2] then
+    multipliers[1] = multipliers[1] * 5
+end
+if game[4][3] then
+    multipliers[2] = multipliers[2] * 2
+end
+if game[4][4] then
+    multipliers[2] = multipliers[2] * 5
+end
+if game[4][5] then
+    multipliers[3] = multipliers[3] * 2
+end
+if game[4][6] then
+    multipliers[3] = multipliers[3] * 5
+end
+if game[4][7] then
+    multipliers[1] = multipliers[1] * 2
+    multipliers[2] = multipliers[2] * 2
+    multipliers[3] = multipliers[3] * 2
+end
+if game[4][8] then
+    multipliers[1] = multipliers[1] * 5
+    multipliers[2] = multipliers[2] * 5
+    multipliers[3] = multipliers[3] * 5
+end
+now = os.time()
+difference = now - game[1]
+game[1] = now
+moneytick = math.floor((game[3][1] * multipliers[2] * difference) + (game[3][2] * multipliers[3] * 0.5 * (difference^2 + difference)) + 0.5)
+game[2] = game[2] + moneytick
+game[6][2] = game[6][2] + moneytick 
+game[3][1] = game[3][1] + (game[3][2] * multipliers[3] * difference)
+multipliers[1] = multipliers[1] * (game[3][1] + 1)
+game[2] = game[2] + multipliers[1]
+game[6][2] = game[6][2] + multipliers[1]
+
+--then, execute any commands
+buildingcosts = {math.floor(10 * (2^game[3][1]) + 0.5), math.floor(1000 * (5^game[3][2]) + 0.5)}
+upgradecosts = {10, 500, 1000, 50000, 1e5, 5e6, 1e7, 5e8}
+affordable = {}
+colors = {"0", "504"}
+for i = 1, #upgradecosts do
+    affordable[i] = game[2] >= upgradecosts[i]
+    if game[4][i] then
+        colors[1] = tostring((i * 64) - 8)
+    end
+    if affordable[i] then
+        colors[2] = tostring(((8 - i) * 64) - 8)
+    end
+end
+
+--now generate the graphic
+chars = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "e", ".", ":", "!"}
+locs = {}
+for i = 1, #chars do
+    locs[chars[i]] = tostring(-48 * (i - 1))
+end
+
+--game graphic
+graphic = "{" .. "iscript: "
+graphic = graphic .. "\nload https://raw.githubusercontent.com/PighouseBacon/NotSoBotIncremental/main/pictures/game%20background.png template"
+graphic = graphic .. "\nload https://raw.githubusercontent.com/PighouseBacon/NotSoBotIncremental/main/pictures/numbers%20spritesheet.png spritesheet"
+graphic = graphic .. "\ncreate cropped 40 40 0 0 0 255"
+--money value
+graphic = graphic .. makeText(game[2], 328, 200)
+--money/sec
+graphic = graphic .. makeText(game[3][1] * multipliers[2], 632, 200)
+--money/click
+graphic = graphic .. makeText(multipliers[1], 936, 200)
+--formers
+graphic = graphic .. makeText(game[3][1], 328, 328)
+--formers cost
+graphic = graphic .. makeText(buildingcosts[1], 632, 328)
+--formers making
+graphic = graphic .. makeText(multipliers[2], 936, 328)
+--makers
+graphic = graphic .. makeText(game[3][2], 328, 456)
+--makers cost
+graphic = graphic .. makeText(buildingcosts[2], 632, 456)
+--makers making
+graphic = graphic .. makeText(multipliers[3], 936, 456)
+--upgrades
+if colors[1] ~= "0" then
+    graphic = graphic .. "\ncreate green " .. colors[1] .. " 56 0 128 0 128"
+    graphic = graphic .. "\noverlay template green 432 512"
+end
+if colors[2] ~= "-8" then
+    graphic = graphic .. "\ncreate black " .. colors[2] .. " 56 0 0 0 128"
+    graphic = graphic .. "\noverlay template black " .. tostring(936 - colors[2]) .. " 512"
+end
+--render
+graphic = graphic .. "\nrender template}\n"
+
+--stats graphic
+graphic = graphic .. "{" .. "iscript: "
+graphic = graphic .. "\nload https://raw.githubusercontent.com/PighouseBacon/NotSoBotIncremental/main/pictures/stats.png stats"
+graphic = graphic .. "\nload https://raw.githubusercontent.com/PighouseBacon/NotSoBotIncremental/main/pictures/numbers%20spritesheet.png spritesheet"
+graphic = graphic .. "\ncreate cropped 40 40 0 0 0 255"
+--render
+graphic = graphic .. "\nrender stats}"
+
+
+
+debug = tabletostring(game)
+print(debug)
+print(encode(debug))
+print(graphic)
